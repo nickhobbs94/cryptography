809d022623082db8fa11e7c0e2e5c516f1ea6b54
Nicholas Hobbs <nick@nhobb.com>
2023-05-13 09:59:47 +1000

    Clamp passes tests

--- poly1305.js
+++ poly1305.js
@@ -1,16 +1,19 @@
 // poly1305
 
 export function octoload(str) {
-    let count = 0;
     let inputs = str.split(':').reverse().map(s => parseInt('0x'+s));
-    let result = new Uint32Array(Math.ceil(inputs.length / 4));
-
-    for (let inByte of inputs) {
-        const currentIndex = Math.floor(count / 4);
-        result[currentIndex] = result[currentIndex] * 0x100 + inByte;
-        count++;
-    }
+    let result = new Uint8Array(inputs);
     return result;
 }
 
+export function clamp(r) {
+    r[15-3] &= 15;
+    r[15-7] &= 15;
+    r[15-11] &= 15;
+    r[15-15] &= 15;
+    r[15-4] &= 252;
+    r[15-8] &= 252;
+    r[15-12] &= 252;
+    return r;
+}
 
